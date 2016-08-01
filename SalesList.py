c1fff8166b8864533ed2ace71a00288eaad004df
Cherishty <david.chen.ty@foxmail.com>
2016-08-01 23:42:19 +0800

    check write file exist,easy to use model excel

--- SalesList.py
+++ SalesList.py
@@ -6,54 +6,55 @@ import XLFormat
 
 def readXls():
     print('\n------------------------------------')
-    print ('销售汇总 v1.2')  # decode via  function
-    print ('请将需要统计的报表放在该目录的data文件夹下,并从0.xls开始顺序命名')
-    print ('请输入待汇总的表格份数')
-    companyList = {}
+    print('销售汇总 v1.2')
+    print('请将需要统计的报表放在该目录的data文件夹下,并从0.xls开始顺序命名')
+    print('请将用于记录结果的 样表 放到当前目录下，注意命名为“销售报表.xls”')
+    print('请输入待汇总的表格份数')
 
+    try:
+        rb = xlrd.open_workbook('销售报表.xls')
+    except:
+        print('没有找到 销售报表.xls，操作失败！请检查您的路径或命名\n\n')
+        return
+
+    companyList = {}
     num = input()
     while num.isalpha():
-        print ('输入错误，请重新输入合法数字')
+        print('输入错误，请重新输入合法数字')
         num = input()
 
     for each in range(int(num)):
         try:
-            data = xlrd.open_workbook('data/' + str(each) + '.xls')
+            data = xlrd.open_workbook('datass/' + str(each) + '.xls')
         except:
-            print ('没有找到该目录或该文件，请检查您的路径或命名\n\n')
-            print ('------------------------------------\n\n')
+            print('没有找到待读取的%d.xls，操作失败！请检查您的路径或命名\n\n' % each)
             return
-        else:
-            table = data.sheets()[0]
-            row = table.nrows
-            for eachRow in range(2, row):
-                companyName = table.cell(eachRow, 2).value
-                if not companyName:
-                    break
-                money = table.cell(eachRow, 6).value
-                if companyList.has_key(companyName):
-                    companyList[companyName] = companyList[companyName] + float(money)
-                else:
-                    companyList[companyName] = float(money)
+
+        print('%d.xls读取成功，正在处理......' % each)
+        table = data.sheets()[0]
+        row = table.nrows
+        for eachRow in range(2, row):
+            companyName = table.cell(eachRow, 2).value
+            if not companyName:
+                break
+            money = table.cell(eachRow, 6).value
+            if companyName in companyList:
+                companyList[companyName] = companyList[companyName] + float(money)
+            else:
+                companyList[companyName] = float(money)
     return companyList
 
 
 def writeXls(sheet):
-    file = xlwt.Workbook(encoding='utf-8')  # encoding='utf-8' to support chinese output
-    table = file.add_sheet('1')
-    i = 0
-    k = 0
-    title = ['公司名称','金额']
-    for each in title:
-        table.write(i, k, each)
-        k += 1
-    i += 1
-
+    rb = xlrd.open_workbook('销售报表.xls', formatting_info=True)
+    wb = copy(rb)
+    table = wb.get_sheet(0)
+    i = 2
     for key in sheet:
-        table.write(i, 0, key)
-        table.write(i, 1, sheet[key])
+        XLFormat.setOutCell(table, 2, i, key)
+        XLFormat.setOutCell(table, 6, i, sheet[key])
         i += 1
-    file.save('result.xls')
+    wb.save('销售报表.xls')
 
 
 if __name__ == '__main__':
@@ -61,4 +62,4 @@ if __name__ == '__main__':
     while not sheet:
         sheet = readXls()
     writeXls(sheet)
-    print ("run successfully!")
+    print("运行成功，结果保存在该目录的 销售报表.xls中")
--- StSalaryDC.py
+++ StSalaryDC.py
@@ -9,8 +9,14 @@ def readXls():
     print('挡车工序 工资计算v1.4')
     print('请将需要统计的报表放在该目录的data文件夹下,并从0.xls开始顺序命名')
     print('请输入待汇总的表格份数')
-    sheet = {}
 
+    try:
+        rb = xlrd.open_workbook('销售报表.xls')
+    except:
+        print('没有找到 工资总表.xls，操作失败！请检查您的路径或命名\n\n')
+        return
+
+    sheet = {}
     num = input()
     while num.isalpha():
         print('输入错误，请重新输入合法数字')
@@ -20,10 +26,11 @@ def readXls():
         try:
             data = xlrd.open_workbook('dataDC/' + str(each) + '.xls')
         except:
-            print('没有找到该目录或该文件，请检查您的路径或命名')
+            print('没有找到待读取的%d.xls，操作失败！请检查您的路径或命名\n\n' % each)
             print('------------------------------------\n\n')
             return
         else:
+            print('%d.xls读取成功，正在处理......' % each)
             table = data.sheets()[0]
             list = []
             cell_B1 = table.cell(0, 9).value
@@ -53,7 +60,7 @@ def readXls():
 
 
 def writeXls(sheet):
-    rb = xlrd.open_workbook('result.xls', formatting_info=True)
+    rb = xlrd.open_workbook('工资总表.xls', formatting_info=True)
     wb = copy(rb)
     table = wb.get_sheet(0)
     i = 3
@@ -70,7 +77,7 @@ def writeXls(sheet):
         XLFormat.setOutCell(table, 23, each, xlwt.Formula('S%d-T%d-U%d' % (i + 1, i + 1, i + 1)))
     for j in range(75, 89):
         XLFormat.setOutCell(table, j - ord('A'), 41, xlwt.Formula('SUM(%c4:%c41)' % (chr(j), chr(j))))
-    wb.save('result.xls')
+    wb.save('工资总表.xls')
 
 
 if __name__ == '__main__':
@@ -78,4 +85,4 @@ if __name__ == '__main__':
     while not sheet:
         sheet = readXls()
     writeXls(sheet)
-    print("run successfully!")
+    print('运行成功，结果保存在该目录的 工资总表.xls中')
